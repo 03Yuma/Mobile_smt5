39017a7b628975ee6a67b3e1f52d489b04631d82
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-09-20 09:34:11 +0700

    minggu 4

---
+++ Pertemuan3/Pertemuan3_Yuma.dart
@@ -0,0 +1,12 @@
+void main(){
+  String test = "test2";
+if(test == "test"){
+  print("Test1");
+}else if(test == "test2"){
+  print("Test2");
+}else{
+  print("Something else");
+}
+
+if(test == "test2") print("Test2 again");
+}
