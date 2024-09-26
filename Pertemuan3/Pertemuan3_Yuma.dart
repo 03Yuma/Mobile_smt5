a99463336270b83b70437b904ba03915489e40fb
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-09-26 17:53:31 +0700

    praktikum 1-3

--- Pertemuan3/Pertemuan3_Yuma.dart
+++ Pertemuan3/Pertemuan3_Yuma.dart
@@ -1,14 +1,58 @@
 void main(){
-  String test = "test2";
-if(test == "test"){
-  print("Test1");
-}else if(test == "test2"){
-  print("Test2");
-}else{
-  print("Something else");
+
+// Praktikum 1
+
+// Langkah 1
+//   String test = "test2";
+// if(test == "test"){
+//   print("Test1");
+// }else if(test == "test2"){
+//   print("Test2");
+// }else{
+//   print("Something else");
+// }
+
+// if(test == "test2") print("Test2 again");
+
+// Langkah 3
+// bool test =true;
+// if(test){
+//   print("Kebenaran");
+// }
+
+// Praktikum 2
+
+// Langkah 1
+
+// int counter =0;
+// // while (counter <33){
+// //   print(counter);
+// //   counter++;
+// // }
+
+// // Lanngkah 3
+// do{
+//   print(counter);
+//   counter++;
+// }while (counter < 77) ;
+
+// Praktikum 3
+
+// langkah 1
+// for (int index = 10; index < 27; index) {
+//   print(index);
+// }
+
+// langkah 3
+
+int index;
+for (index = 10; index < 27; index++) {
+if (index == 21){
+  break;
+}else if(index > 1 || index<7){
+  print(index);
 }
 
-if(test == "test2") print("Test2 again");
-
+}
 }
 
--- Pertemuan3/README.md
+++ Pertemuan3/README.md
@@ -1,6 +1,25 @@
 #Praktikum 1
 
-##jawaban langkah 2
+##Langkah 2
 
 yang terjadi adalah eror, dikarenakan pada dart if else harus menggunakan format huruf kecil
 
+##Langkah 3
+eror karna if else hanya menerima tipe data boolean
+
+#Praktikum 2
+
+##Langkah 2
+
+variabel counter belum dideklarasi
+
+##Langkah 3
+pada kode yang diberikan variabel counter belum dideklarasi
+
+#Praktikum 3
+
+##Langkah 2
+Pada kode diatas memiliki kesalahan yaitu variabel yang tidak konsisten dan juga variabel belum dinisialisasi
+
+##Langkah 3
+eror terjadi karena terdapat tidak konsistennya variabel, dan adannya break dan continue yang tidak didalam looping
