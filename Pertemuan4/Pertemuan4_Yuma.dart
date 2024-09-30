d61471a640af0bd1e72f6e1e7919be95d91e98cb
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-09-30 18:12:50 +0700

    praktikum selesai

--- Pertemuan4/Pertemuan4_Yuma.dart
+++ Pertemuan4/Pertemuan4_Yuma.dart
@@ -65,42 +65,105 @@ void main(){
 
 // Langkah 3
 
-var gifts = Map<String, String>();
-gifts['first'] = 'Yuma';
-gifts['second'] = 'turtledoves';
-gifts['fifth'] = 'golden rings';
+// var gifts = Map<String, String>();
+// gifts['first'] = 'Yuma';
+// gifts['second'] = 'turtledoves';
+// gifts['fifth'] = 'golden rings';
 
-gifts['nama'] = 'Yuma Rakha';
-gifts['NIM'] = '2241720194';
+// gifts['nama'] = 'Yuma Rakha';
+// gifts['NIM'] = '2241720194';
 
-var nobleGases = Map<int, String>();
-nobleGases[2] = 'helium';
-nobleGases[10] = 'neon';
-nobleGases[18] = 'argon';
+// var nobleGases = Map<int, String>();
+// nobleGases[2] = 'helium';
+// nobleGases[10] = 'neon';
+// nobleGases[18] = 'argon';
 
-nobleGases[3] = 'Yuma Rakha ';
-nobleGases[5] = '2241720194 ';
+// nobleGases[3] = 'Yuma Rakha ';
+// nobleGases[5] = '2241720194 ';
 
-var mhs1 = Map<String, String>();
-mhs1['first'] = 'Yuma';
-mhs1['second'] = 'turtledoves';
-mhs1['fifth'] = 'golden rings';
+// var mhs1 = Map<String, String>();
+// mhs1['first'] = 'Yuma';
+// mhs1['second'] = 'turtledoves';
+// mhs1['fifth'] = 'golden rings';
 
-mhs1['nama'] = 'Yuma Rakha';
-mhs1['NIM'] = '2241720194';
+// mhs1['nama'] = 'Yuma Rakha';
+// mhs1['NIM'] = '2241720194';
 
-var mhs2 = Map<int, String>();
-mhs2[2] = 'helium';
-mhs2[10] = 'neon';
-mhs2[18] = 'argon';
+// var mhs2 = Map<int, String>();
+// mhs2[2] = 'helium';
+// mhs2[10] = 'neon';
+// mhs2[18] = 'argon';
 
-mhs2[6] = 'Yuma Rakha ';
-mhs2[7] = '2241720194 ';
+// mhs2[6] = 'Yuma Rakha ';
+// mhs2[7] = '2241720194 ';
 
-print("Gifts: $gifts");
-print("Noble Gases: $nobleGases");
-print("mhs1: $mhs1");
-print("mhs2: $mhs2");
+// print("Gifts: $gifts");
+// print("Noble Gases: $nobleGases");
+// print("mhs1: $mhs1");
+// print("mhs2: $mhs2");
 
+// Praktikum 4
+// langkah 1
+
+// var list = [1,2,3];
+// var list2 = [0,...list];
+// print(list);
+// print(list2);
+// print(list2.length);
+
+// langkah 3
+
+// var list1 = [1,2, null];
+// print(list1);
+// var list3 = [0,...?list1];
+// print(list3.length);
+
+// var list4 = ["Yuma Rakha","/","2241720194"];
+// print(list4);
+
+// langkah 4
+// bool promoActive = true;
+// bool promoActive = false;
+// var nav = ['Home','Furniture','Plants', if (promoActive) 'Outlet'];
+// print(nav);
+
+// langkah 5
+// String login = 'Manager';
+// var nav2 = ['Home','Furniture','Plants',if (login == 'Manager')'Inventory'];
+// print(nav2);
+
+// langkah 6
+// var listOfInts = [1,2,3];
+// var listOfStrings = ['#0', for(var i in listOfInts)'#$i'];
+// assert(listOfStrings[1]=='#1');
+// print(listOfStrings);
 }
 
+// Praktikum 5
+
+// langkah 1
+
+// var record= ('first', 2, true,'last');
+// print(record);
+
+// langkah 3
+ (int, int) tukar((int, int) record){
+  var (a,b) = record;
+  return (b,a);
+
+  // langkah 4
+  // Record type annotation in a variable declaration:
+  // (String, int) mahasiswa = ('Yuma Rakha', 2241720194); 
+  // print(mahasiswa); 
+
+  // langkah 5
+
+//  var mahasiswa2 = ('Yuma Rakha', a: 2241720194, b: true, 'last');
+
+//   print(mahasiswa2.$1); // Prints 'Yuma Rakha'
+//   print(mahasiswa2.a); // Prints 2241720194
+//   print(mahasiswa2.b); // Prints true
+//   print(mahasiswa2.$2); // Prints 'last'
+
+
+ }
--- Pertemuan4/README.md
+++ Pertemuan4/README.md
@@ -16,9 +16,31 @@ tidak terjadi eror, kode menampilkan variabel halogens
 
 kode memberikan output dari variabel map nobleGases dimana ini bertipe int dan variabel map gifts dimana bertipe string. tiap key dan value dari variabel memiliki tipe data yang berbeda
 
-# Langkah 3
+## Langkah 3
 
 terjadi eror, karna kode ingin melakukan deklrasi mhs 1 sebagai gifts maupun nobleGases akan tetapi tidak ada proses deklarasinya. kode memberikan nama variabel yang salah
+
+# Praktikum 4
+## langkah 2
+
+eror dikarenakan tidak ada variabel list1. sehingga list1 maupun list2 tidak dapat dicetak. list2 tidak dapat dicetak dikarenakan menggunakan spread operator
+
+## langkah 3
+
+terjadi eror dikarenakan varibel list1 belum memiliki tipe data. 
+
+## langkah 4
+
+memanimalisir kesalahan dari penggunaan pengulangan kode yang sama
+
+# Praktikum 5
+## langkah 4
+
+terjadi eror karna variabel mahasiswa yangbelum diinisilisasi dan dideklarasi
+
+## langkah 5
+terjadi eror karna salah pendefinisian pada record
+
 
 
 
