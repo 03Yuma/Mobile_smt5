5f435d0bf130d7049967c500fd679a7beee136f1
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-10-20 12:29:41 +0700

    praktikum 1-4

--- layout_flutter/layout_flutter/lib/main.dart
+++ layout_flutter/layout_flutter/lib/main.dart
@@ -8,64 +8,112 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /* soal 1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /* soal 2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: const Text(
-                  'Batu, Malang, Indonesia',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Batu, Malang, Indonesia',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              const Text(
-                'Batu, Malang, Indonesia',
-                style: TextStyle(
-                  color: Colors.grey),
-              ),
-            ],
+                const Text(
+                  'Kota wisata di Jawa Timur',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        /* soal 3*/
-        const Icon(
-        Icons.star,
-          color: Colors.red,
-        ),
-        const SizedBox(width: 8),
-        const Text(
-          '41',
-          style: TextStyle(
-            fontSize: 16,
+          const Icon(
+            Icons.star,
+            color: Colors.red,
           ),
-        ),
+          const SizedBox(width: 8),
+          const Text(
+            '41',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
       ],
-    ),
-  );
-  
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Sungai Brantas adalah sebuah sungai yang mengalir di provinsi Jawa Timur, Indonesia. Sungai ini adalah sungai' 
+        'terpanjang kedua di Pulau Jawa setelah Bengawan Solo hingga tahun 2015, terdapat 18,166 juta orang yang tinggal'
+        'di wilayah sungai ini atau 46,7% dari total penduduk Jawa Timur. Sungai Brantas juga berperan penting dalam menunjang'
+        'status Jawa Timur sebagai lumbung pangan nasional.'
+
+        '\nYuma Rakha Samodra Sikayo\n'
+        '2241720194',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Yuma Rakha Samodra Sikayo dan 2241720194',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        body: ListView(
           children: [
-            titleSection, 
-            const Center(
-              child: Text('Hello World'), 
+            Image.asset(
+              'images/Brantas.jpeg', 
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
+            titleSection,
+            buttonSection,
+            textSection, 
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
