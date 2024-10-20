0deb0ec28d6edb507d41705b93dbad0a69949b56
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-10-20 09:30:56 +0700

    praktikum 1

--- layout_flutter/layout_flutter/lib/main.dart
+++ layout_flutter/layout_flutter/lib/main.dart
@@ -7,14 +7,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Text(
+                'Batu, Malang, Indonesia',
+                style: TextStyle(
+                  color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+        /* soal 3*/
+        const Icon(
+        Icons.star,
+          color: Colors.red,
+        ),
+        const SizedBox(width: 8),
+        const Text(
+          '41',
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ],
+    ),
+  );
+  
     return MaterialApp(
-      title: 'Flutter layout: Nama dan NIM Anda',
+      title: 'Flutter layout: Yuma Rakha Samodra Sikayo dan 2241720194',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            titleSection, 
+            const Center(
+              child: Text('Hello World'), 
+            ),
+          ],
         ),
       ),
     );
